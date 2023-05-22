fd4254e2315e169c732745f35b65fbb5b077ea7a
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-22 10:57:06 +0900

    fix

---
+++ tor/index.ts
@@ -0,0 +1,37 @@
+export interface TorOptions {
+  hostname?: string
+  torCommand?: string
+}
+export class Tor{
+  hostname: string
+  torCommand: string
+  constructor(options: TorOptions){
+    this.hostname = options.hostname ? options.hostname : "127.0.0.1:9050"
+    if(options.torCommand){
+      this.torCommand = options.torCommand
+    }else{
+      if(Deno.build.os === "windows"){
+        // OS: windows
+        this.torCommand = "tor.exe"
+      }else{
+        this.torCommand = "tor"
+      }
+    }
+
+  }
+  async start(){
+    const torProcess = new Deno.Command(this.torCommand, {
+      
+    })
+    torProcess.spawn()
+  }
+  async get(url: string): Response{
+    const curl = new Deno.Command("curl", {
+      args: [
+        "https://google.com"
+      ]
+    })
+    const res = new Response(curl)
+    return res
+  }
+}
