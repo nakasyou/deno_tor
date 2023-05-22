5de930161f403ec4d9850489e9f7a94f5bb9da9f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-22 12:12:52 +0900

    fix

--- tor/parseHeader.ts
+++ tor/parseHeader.ts
@@ -5,8 +5,13 @@ export default (raw: string) => {
 
   const headerData: Array<string> = (datas.at(-1) === "" ? datas.at(-2) : datas.at(-1)).split("\n")
 
-  const statusCode = parseInt(headerData[0].split(" ")[1])
+  const statusCode = parseInt(headerData[0].split(" ")[1])  // Get status code
+  
+  const headers = statusCode.slice(1).map(header=>{
+    const headerSplit = header.split(": ")
+    return [headerSplit[0],headerSplit.slice(1).join(": ")]
+  })
 
-  console.log(statusCode)
+  console.log()
   
 }
