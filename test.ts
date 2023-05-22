8e17d3ee938aac31da926f314dcc5eadac067c5b
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-22 12:41:23 +0900

    https -> http

--- test.ts
+++ test.ts
@@ -2,6 +2,6 @@ import { Tor } from "./mod.ts"
 
 const tor = new Tor()
 
-const res = await tor.get("https://google.com")
+const res = await tor.get("http://google.com")
 console.log(res)
 //console.log(await res.text())
