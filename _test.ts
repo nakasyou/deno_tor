db34195f3a771e56211f8cc80b244d211fe3037a
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-22 15:50:15 +0900

    Update and rename test.ts to _test.ts

--- _test.ts
+++ _test.ts
@@ -2,6 +2,8 @@ import { Tor } from "./mod.ts"
 
 const tor = new Tor()
 
+await tor.start()
+
 const res = await tor.fetch("https://www.bbcweb3hytmzhn5d532owbu6oqadra5z3ar726vq5kgwwn6aucdccrad.onion", {
   method: "get"
 })
