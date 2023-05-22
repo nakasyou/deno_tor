11f21a9fd619ff81dbbc2661e9e8f31a2fbb70bb
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-22 15:30:16 +0900

    fix testcode

--- test.ts
+++ test.ts
@@ -3,9 +3,8 @@ import { Tor } from "./mod.ts"
 const tor = new Tor()
 
 const res = await tor.fetch("https://www.bbcweb3hytmzhn5d532owbu6oqadra5z3ar726vq5kgwwn6aucdccrad.onion", {
-  method: "post",
-  body: "Hello!"
+  method: "get"
 })
 console.log(res)
-console.log(res.text())
+console.log(await res.text())
 //console.log(await res.text())
