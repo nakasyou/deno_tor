e19c5cb9638759b4d34ed0137dbe1f49799cb6f5
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-22 15:18:53 +0900

    test fix

--- test.ts
+++ test.ts
@@ -2,6 +2,9 @@ import { Tor } from "./mod.ts"
 
 const tor = new Tor()
 
-const res = await tor.get("http://google.com")
+const res = await tor.fetch("http://google.com", {
+  method: "post",
+  body: "Hello!"
+})
 console.log(res)
 //console.log(await res.text())
